73eabb26bf29ed541ca95b3b28f1a763a5db0e56
sheep <sheep@sheepdev.xyz>
2023-04-06 18:44:48 +0200

    fix

--- programs/scripts/bitwarden.sh
+++ programs/scripts/bitwarden.sh
@@ -4,6 +4,4 @@ mkdir $HOME/.config/nxm/bitwarden
 cd $HOME/.config/nxm/bitwarden
 
 curl -Lso bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
-cd $HOME/.config/nxm/bitwarden
-echo "Please run ./bitwarden.sh install to finish the installation."
-exit
+tmux new-session -d -s "bitwarden" ./bitwarden.sh install
