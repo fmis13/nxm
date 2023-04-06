014cab76d49db9a85fc58fc28899500f50519f70
sheep <sheep@sheepdev.xyz>
2023-04-06 19:17:42 +0200

    fix

--- programs/scripts/bitwarden.sh
+++ programs/scripts/bitwarden.sh
@@ -7,4 +7,4 @@ PATH=$HOME/.config/nxm/bitwarden
 
 curl -Lso $PATH/bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
 echo "Please run ./bitwarden.sh install from the command line and fill in all the prompts, then follow the guide at https://bitwarden.com/help/install-on-premise-linux/"
-cd $PATH
+cd PATH
