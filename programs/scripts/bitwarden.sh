db75df161fa1cf66f9c82a4ba037de23498713e5
sheep <sheep@sheepdev.xyz>
2023-04-06 18:49:16 +0200

    fix

--- programs/scripts/bitwarden.sh
+++ programs/scripts/bitwarden.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir $HOME/.config/nxm/bitwarden
+mkdir -p $HOME/.config/nxm/bitwarden
 cd $HOME/.config/nxm/bitwarden
 
 curl -Lso bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
