7b00c6900a1e4489540a6a227b01bcd58d10192a
sheep <sheep@sheepdev.xyz>
2023-04-06 17:46:35 +0200

    urgent part 2

--- programs/scripts/bitwarden.sh
+++ programs/scripts/bitwarden.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-adduser bitwarden --disabled-password
-usermod -aG docker bitwarden
-mkdir /opt/bitwarden
-chmod -R 700 /opt/bitwarden
-chown -R bitwarden:bitwarden /opt/bitwarden
-runuser -l bitwarden -c 'curl -Lso bitwarden.sh https://go.btwrdn.co/bw-sh && chmod 700 bitwarden.sh'
-runuser -l bitwarden -c './bitwarden.sh install'
-runuser -l bitwarden -c './bitwarden.sh start'
+mkdir $HOME/.config/nxm/bitwarden
+cd $HOME/.config/nxm/bitwarden
+curl -Lso bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh && ./bitwarden.sh install
