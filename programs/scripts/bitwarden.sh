ccf201775fb41fb779e453d21f62f80c80b5b7cc
sheep <sheep@sheepdev.xyz>
2023-04-06 18:00:55 +0200

    fixing installation scripts

--- programs/scripts/bitwarden.sh
+++ programs/scripts/bitwarden.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+cat << "EOF"
+ _     _ _                         _            
+| |__ (_) |___      ____ _ _ __ __| | ___ _ __  
+| '_ \| | __\ \ /\ / / _` | '__/ _` |/ _ \ '_ \ 
+| |_) | | |_ \ V  V / (_| | | | (_| |  __/ | | |
+|_.__/|_|\__| \_/\_/ \__,_|_|  \__,_|\___|_| |_|
+EOF
+
+cat << EOF
+Open source password management solutions
+Copyright 2015-$(date +'%Y'), 8bit Solutions LLC
+https://bitwarden.com, https://github.com/bitwarden
+===================================================
+EOF
+
 mkdir $HOME/.config/nxm/bitwarden
 cd $HOME/.config/nxm/bitwarden
-curl -Lso bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh && ./bitwarden.sh install
+
+curl -Lso bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
+./bitwarden.sh install
