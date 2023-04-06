f217d520139f062428f45dff80e9bdf5f3381e90
sheep <sheep@sheepdev.xyz>
2023-04-06 19:05:01 +0200

    fix

--- programs/scripts/bitwarden.sh
+++ programs/scripts/bitwarden.sh
@@ -6,6 +6,7 @@ mkdir -p $HOME/.config/nxm/bitwarden
 cd $HOME/.config/nxm/bitwarden
 
 curl -Lso bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
+./bitwarden.sh install
 while true; do
     if ! pgrep bash; then
         break
