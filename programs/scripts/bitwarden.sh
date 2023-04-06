39dc445717e8aa2ff57fcfc410aeedfa6de8c726
sheep <sheep@sheepdev.xyz>
2023-04-06 19:16:34 +0200

    fix

--- programs/scripts/bitwarden.sh
+++ programs/scripts/bitwarden.sh
@@ -3,7 +3,8 @@
 set -e
 
 mkdir -p $HOME/.config/nxm/bitwarden
-cd $HOME/.config/nxm/bitwarden
+PATH=$HOME/.config/nxm/bitwarden
 
-curl -Lso bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
+curl -Lso $PATH/bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
 echo "Please run ./bitwarden.sh install from the command line and fill in all the prompts, then follow the guide at https://bitwarden.com/help/install-on-premise-linux/"
+cd $PATH
