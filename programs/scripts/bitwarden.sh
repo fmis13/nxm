4c925e84ca3bd4e6c33a7f2b588c365930ee8fe5
sheep <sheep@sheepdev.xyz>
2023-04-06 19:37:55 +0200

    fix

--- programs/scripts/bitwarden.sh
+++ programs/scripts/bitwarden.sh
@@ -6,14 +6,4 @@ mkdir -p $HOME/.config/nxm/bitwarden
 PATH=$HOME/.config/nxm/bitwarden
 
 curl -Lso $PATH/bitwarden.sh "https://func.bitwarden.com/api/dl/?app=self-host&platform=linux" && chmod 700 bitwarden.sh
-
-./bitwarden.sh install
-pid=$!
-
-trap "kill $pid 2> /dev/null" EXIT
-
-while kill -0 $pid 2> /dev/null; docker
-    sleep 1
-done
-
-trap - EXIT
+echo "Run $HOME/.config/nxm/bitwarden/bitwarden.sh install to finish the installation"
